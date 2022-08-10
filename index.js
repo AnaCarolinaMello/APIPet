00d2db49ad2e0033a600a5e4cf8b7bc3821dcbec
Cakeforever <eusoueu.carol@gmail.com>
2022-08-10 14:46:00 -0300

    Deletar e buscar por id funcionadndo

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+const indexRoute = require('./routes/route');
 const express = require('express');
 const app = express()
 const bodyParser = require('body-parser')
@@ -7,7 +8,7 @@ const path = require('path')
 const fs = require('fs')
 const cors = require('cors')
 const controller = require("./controllers/dataController")
-// const indexRoute = require('./routes/route');
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')) 
@@ -15,43 +16,14 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'views')));
-app.use('/public', express.static('public'))
+app.use(express.static('public'))
 
 global.fileName = "fichas.json";
-// const { readFile, writeFile} = fs;
-//var id = 1;
 
-// app.get('/',controller.getFichas);
-
-// app.post()
-
-// app.post('/index', (req,res) =>{
-//     const nome = req.body.nomeDoAnimal;
-//     const idade = req.body.idadeDoAnimal;
-//     const peso = req.body.pesoDoAnimal;
-//     const porte = req.body.porteDoAnimal;
-//     const raca = req.body.racaDoAnimal;
-//     // const ficha = [nome,idade,peso,porte,raca]
-    // res.send(`<!DOCTYPE html>
-    // <html lang="en">
-    // <head>
-    //     <meta charset="UTF-8">
-    //     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    //     <title>Document</title>
-    //     <link rel="stylesheet" href="../public/style/style.css" type="text/css">
-    // </head>
-    // <body>
-    //     <main>
-    //         <label>${nome}</label>     
-    //     </main>
-    // </body>
-    // </html>`)
-// })
 
 app.post('/index',async(req,res,next) =>{
     try{
-        //id++
+        
         const nome = req.body.nomeDoAnimal;
         const idade = req.body.idadeDoAnimal;
         const peso = req.body.pesoDoAnimal;
@@ -67,42 +39,52 @@ app.post('/index',async(req,res,next) =>{
             porte: porte,
             raca: raca
         }
-        // console.log(id)
         data.fichas.push(ficha)
         await fs.writeFileSync(global.fileName,JSON.stringify(data, null, 2))
         res.send(ficha)
         console.log(ficha)
-        // res.send(`<!DOCTYPE html>
-        //             <html lang="en">
-        //                 <head>
-        //                     <meta charset="UTF-8">
-        //                     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        //                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        //                     <title>Document</title>
-        //                     <link rel="stylesheet" href="../public/style/style.css" type="text/css">
-        //                 </head>
-        //                 <body>
-        //                     <main>
-        //                         <label>${nome}</label>     
-        //                     </main>
-        //                 </body>
-        //             </html>`)
     }catch(err){
         next(err)
     }
 });
 
+app.get("/getFicha",async(req,res)=>{
+    const data = JSON.parse(await fs.readFileSync(global.fileName));
+    res.send(data.fichas)
+})
 
+app.get("/:id",async(req,res,next)=>{
+    try{
+        const data = JSON.parse(await fs.readFileSync(global.fileName))
+        console.log(data)
+        const ficha = data.fichas.find(
+            ficha => ficha.id === parseInt(req.params.id)
 
-// app.post('/models/dataFicha', function(req, res) {
-    
-// });
+        );
+        res.send(ficha)
+        console.log("Buscar pet pelo id")
+    }catch(err){
+        next(err)
+    }
+})
+
+app.delete("/:id", async (req,res,next) =>{
+    try{
+        const data = JSON.parse(await fs.readFileSync(global.fileName))
+        data.fichas = data.fichas.filter(
+            fichas => fichas.id !== parseInt(req.params.id)
+        );
+        await fs.writeFileSync(global.fileName, JSON.stringify(data, null, 2));        
+        res.end();
+        console.log("Pet deletado")
+    }catch(err){
+        next(err)
+    }
+})
 
 app.get('/', controller.getGatos);
 
 app.get('/', controller.getCao);
-
-app.get('/', controller.getFichas);
 
 app.listen(port, err =>{
     console.log(`http://localhost:${port}`)
--- routes/route.js
+++ routes/route.js
@@ -1,86 +1,21 @@
+
 const express = require('express') 
 const router = express.Router();
 const bodyParser = require('body-parser')
 const dataController = require("../controllers/dataController");
-// const data = require('../models/dataModels');
-app.use(express.json());
 const fs = require('fs')
-app.use(bodyParser.json());
-global.fileName = "teste.json";
-const { readFile, writeFile} = fs;
-var id = 1;
-// const gato = require("../models/dataModels")
+global.fileName = "fichas.json";
 
 router.get('route.js', (req, res, next)=>{
     const gatos = dataController.getGatos()
     const caes= dataController.getCao()
-    //const fichas = dataController.getFichas()
     console.log(gatos)
     console.log(fichas)
     res.render('route',{
         cao: caes,
         gato:gatos
-        //ficha: fichas
     });
 });
 
-// router.get("/:id", async (req,res,next) =>{
-//     try{
-//         const data = JSON.parse(await fs.readFileSync(global.fileName))
-//         const ficha = data.fichas.find(
-//             ficha => ficha.id === parseInt(req.params.id)
-//         );
-//         console.log("Buscar pet pelo id")
-//     }catch(err){
-//         next(err)
-//     }
-// })
-
-// router.delete("/:id", async (req,res,next) =>{
-//     try{
-//         const data = JSON.parse(await fs.readFileSync(global.fileName))
-//         data.fichas = data.fichas.filter(
-//             fichas => fichas.id !== parseInt(req.params.id)
-//         );
-//         await fs.writeFileSync(global.fileName, JSON.stringify(data, null, 2));        
-//         res.end();
-//         console.log("Pet deletado")
-//     }catch(err){
-//         next(err)
-//     }
-// })
-
-
-// router.post('/',async(req,res,next) =>{
-// console.log("teste")
-//     try{
-//         id++
-//         const nome = req.body.nomeDoAnimal;
-//         const idade = req.body.idadeDoAnimal;
-//         const peso = req.body.pesoDoAnimal;
-//         const porte = req.body.porteDoAnimal;
-//         const raca = req.body.racaDoAnimal;
-//         const data = JSON.parse(await readFile(global.fileName));
-//         res.send(data)
-//         const ficha = {
-//             id: id,
-//             nome: nome,
-//             idade: idade,
-//             peso: peso,
-//             porte: porte,
-//             raca: raca
-//         }
-//         data.fichas.push(ficha)
-//         await writeFile(global.fileName,JSON.stringify(data, null, 2))
-//         res.send(ficha)
-//         console.log(ficha)
-//     }catch(err){
-//         next(err)
-//     }
-// });
-
-// router.get('/ficha', function(req, res) {
-//     res.render('path/to/ejs/files/ficha');
-//   });
 
 module.exports = router;
